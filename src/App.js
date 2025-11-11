aa287d82063749235247e8ff96fd12b3bbef0d90
ashna.srivastava <ashna.srivastava@datadoghq.com>
2025-11-10 23:36:51 -0500

    scroll anchor

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CaseStudies from './components/CaseStudies';
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#0a0f0a] flex relative overflow-hidden">
+    <div className="min-h-screen bg-[#0a0f0a] relative overflow-hidden">
       {/* Animated green background effects */}
       <div className="fixed inset-0 pointer-events-none overflow-hidden">
         <div className="absolute top-0 left-0 w-[800px] h-[800px] bg-emerald-500/10 rounded-full blur-[120px] green-pulse"></div>
@@ -12,14 +12,19 @@ function App() {
         <div className="absolute top-1/2 left-1/2 w-[700px] h-[700px] bg-teal-500/5 rounded-full blur-[100px] green-pulse" style={{ animationDelay: '2s' }}></div>
       </div>
       
-      {/* Fixed Left Sidebar */}
-      <div className="w-full lg:w-2/5 xl:w-1/3 fixed lg:sticky top-0 h-screen overflow-hidden relative z-10">
-        <Hero />
-      </div>
-      
-      {/* Right Content - Scrollable */}
-      <div className="w-full lg:w-3/5 xl:w-2/3 lg:ml-auto relative z-10">
-        <CaseStudies />
+      <div className="flex flex-col lg:flex-row">
+        {/* Fixed Left Sidebar */}
+        <div className="w-full lg:w-2/5 xl:w-1/3 lg:fixed lg:top-0 lg:left-0 lg:h-screen overflow-hidden relative z-10">
+          <Hero />
+        </div>
+        
+        {/* Spacer to prevent content from going under fixed sidebar */}
+        <div className="hidden lg:block lg:w-2/5 xl:w-1/3 flex-shrink-0"></div>
+        
+        {/* Right Content - Scrollable */}
+        <div className="w-full lg:w-3/5 xl:w-2/3 relative z-10">
+          <CaseStudies />
+        </div>
       </div>
     </div>
   );
