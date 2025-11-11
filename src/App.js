bdf0849d18313104d92f3491351d9a3656dc38b1
ashna.srivastava <ashna.srivastava@datadoghq.com>
2025-11-11 12:52:45 -0500

    github page changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
 import CaseStudies from './components/CaseStudies';
 import BambuLabCaseStudy from './components/BambuLabCaseStudy';
